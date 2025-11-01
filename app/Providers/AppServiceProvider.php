dbfc36c7b1c6c02c0107a5f76232d12f48d097ff
Arjon Jason Castro <ajcastro29@gmail.com>
2025-11-01 12:36:16 +0800

    Configure default label and validation attribute in Field and Table Column and ImportColumn

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Str;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,41 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        \Filament\Forms\Components\Field::configureUsing(
+            function (\Filament\Forms\Components\Field $component) {
+                $component->label(
+                    fn($component) => Str::title(str_replace('_', ' ', $component->getName()))
+                );
+                $component->validationAttribute(
+                    fn($component) => Str::lower($component->getLabel())
+                );
+            }
+        );
+
+        \Filament\Tables\Columns\Column::configureUsing(
+            function (\Filament\Tables\Columns\Column $column) {
+                $column->label(
+                    fn($column) => Str::title(str_replace('_', ' ', $column->getName()))
+                );
+
+                if (method_exists($column, 'validationAttribute')) {
+                    $column->validationAttribute(
+                        fn($column) => Str::lower($column->getLabel())
+                    );
+                }
+            }
+        );
+
+
+        \Filament\Actions\Imports\ImportColumn::configureUsing(
+            function (\Filament\Actions\Imports\ImportColumn $component) {
+                $component->label(
+                    fn($component) => Str::title(str_replace('_', ' ', $component->getName()))
+                );
+                $component->validationAttribute(
+                    fn($component) => Str::lower($component->getLabel())
+                );
+            }
+        );
     }
 }
